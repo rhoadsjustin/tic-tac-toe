163d6b6948afbd17a1205a62db4ac00a24c79dc1
Justin Rhoads <rhoadsjustin@gmail.com>
2017-05-07 16:05:30 -0500

    created draw function, edited some css, and created moveCount function.

--- js/app.js
+++ js/app.js
@@ -12,6 +12,8 @@ var playerOne = 'x';
 var playerTwo = 'o';
 var currPlayer = playerOne;
 
+var moveCount = 0;
+
 var bx1 = $('#box1');
 var bx2 = $('#box2');
 var bx3 = $('#box3');
@@ -22,69 +24,82 @@ var bx7 = $('#box7');
 var bx8 = $('#box8');
 var bx9 = $('#box9');
 
+//displays a winner or if there is a draw prompting the players to play again.
+function playerOneWins() {
+  $('.jumbotron').removeClass('hidden').prepend('<h1 id="p1wins">Player One wins!</h1>');
+}
+
+function playerTwoWins() {
+  $('.jumbotron').removeClass('hidden').prepend('<h1 id="p2wins">Player Two wins!</h1>');
+}
+
+function draw() {
+  if(moveCount >= 9) {
+  $('.jumbotron').removeClass('hidden').prepend('<h1 id="draw">Its a draw!</h1>');
+  }
+}
+
+// all 16 different win conditions
   function winConditions() {
     if((bx1.hasClass('x') && bx2.hasClass('x')) && bx3.hasClass('x')){
-        $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
-        resetBoard();
+          playerOneWins();
+          resetBoard();
     } else if((bx1.hasClass('o') && bx2.hasClass('o')) && bx3.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx4.hasClass('x') && bx5.hasClass('x')) && bx6.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx4.hasClass('o') && bx5.hasClass('o')) && bx6.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx7.hasClass('x') && bx8.hasClass('x')) && bx9.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx7.hasClass('o') && bx8.hasClass('o')) && bx9.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx1.hasClass('x') && bx4.hasClass('x')) && bx7.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx1.hasClass('o') && bx4.hasClass('o')) && bx7.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx2.hasClass('x') && bx5.hasClass('x')) && bx8.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx2.hasClass('o') && bx5.hasClass('o')) && bx8.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx3.hasClass('x') && bx6.hasClass('x')) && bx9.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx3.hasClass('o') && bx6.hasClass('o')) && bx9.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx1.hasClass('x') && bx5.hasClass('x')) && bx9.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx1.hasClass('o') && bx5.hasClass('o')) && bx9.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     } else if((bx3.hasClass('x') && bx5.hasClass('x')) && bx7.hasClass('x')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player One wins!</h1>');
+      playerOneWins();
       resetBoard();
     } else if((bx3.hasClass('o') && bx5.hasClass('o')) && bx7.hasClass('o')){
-      $('.jumbotron').removeClass('hidden').prepend('<h1>Player Two wins!</h1>');
+      playerTwoWins();
       resetBoard();
     }
-    // } else if (!$('.col-md-4').hasClass('x' || 'o')) {
-		//     alert("Draw! Try playing again!");
-		//     resetBoard();
-	  // }
   }
-
+//resets the board back to its original state
   function resetBoard() {
     $('.col-md-4').removeClass('o');
     $('.col-md-4').removeClass('x');
     currPlayer = playerOne;
+    moveCount = 0;
   }
 
-
+// changes the player after each click
   function changePlayer(){
   if(currPlayer === playerOne) {
       currPlayer = playerTwo;
@@ -92,22 +107,28 @@ var bx9 = $('#box9');
       currPlayer = playerOne;
     }
   }
-
+// click function that runs the game, checks for a winner, and increases the move count to check for a draw
   $('.col-md-4').click(function play() {
     if( $(this).hasClass('x') || $(this).hasClass('o') ){
       alert("Already clicked!");
     } else {
-      $(this).addClass(currPlayer);
+      moveCount++;
+      $(this).addClass(currPlayer, 'clicked').prepend('<h1 class="text-center currLetter">'+currPlayer+'</h1>');
       changePlayer();
       winConditions();
-      }
-
+      draw();
+      console.log(moveCount);
+    }
   });
 
+// click function set on the buttons to make the game run the reset function;
   $('#reset,#reset2').on('click', function() {
   	resetBoard();
     $('.jumbotron').addClass('hidden');
-    $('.jumbotron').remove(h1);
+    $('#p1wins').remove();
+    $('#p2wins').remove();
+    $('#draw').remove();
+    $('.currLetter').remove();
   })
 
 });
